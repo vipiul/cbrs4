67e7185b099869f10373ef5a69999dc1edffad9f
sn0727 <139630877+sn0727@users.noreply.github.com>
2023-08-05 10:50:35 +0530

    I have complete sign up form

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,50 +1,67 @@
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import axios from 'axios';
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+
+// initialvalue of the input feild
+const initialValues = {
+    name: "",
+    email: "",
+    password: ""
+}
+
+const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Please enter your name'),
+    email: Yup.string().email('Invalid email').required('Please enter your email'),
+    password: Yup.string().min(6).required("Please enter your password")
+});
 
 const Register = () => {
     const { push } = useRouter();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [store, setStore] = useState({})
 
-    // resgit api
-    const registerAPi = async () => {
-        const fd = new FormData();
-        fd.append('name', name);
-        fd.append('email', email);
-        fd.append('password', password);
 
-        axios.post('https://sndigitech.in/cbrs/api/register', {
-            data: fd,
+    // I am calling api form the server
+    const ResigerApi = async (value) => {
+        var formData = new FormData();
+        formData.append('name', value.name);
+        formData.append('email', value.email);
+        formData.append('password', value.password);
+
+        axios({
+            method: "post",
+            url: "https://sndigitech.in/cbrs/api/register",
+            data: formData,
             headers: {
-                "Content-Type": "multipart/form-data",
-                "Access-Control-Allow-Origin": "*",
+                'Content-Type': `multipart/form-data;`,
             }
         }).then((response) => {
-                console.log(response);
-            }, (error) => {
-                console.log(error);
-        });
-    }
-    // submit form
-    const SignUp = (e) => {
-        e.preventDefault();
-        // setStore({name: name, email: email, password: password})
-        registerAPi()
+            console.log(response);
+        }).catch((response) => {
+            console.log(response);
+        })
+
     }
 
-    const handlername = (event) => {
-        setName(event.target.value)
-    }
-    const handleremail = (event) => {
-        setEmail(event.target.value)
-    }
-    const handlerpassword = (event) => {
-        setPassword(event.target.value)
-    }
+    // handle form submit 
+    const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+        initialValues: initialValues,
+        validationSchema: SignupSchema,
+        onSubmit: ((value, action) => {
+            if(value) {
+                alert("Login Successfully.")
+                ResigerApi(value)
+            }else {
+                console.log("Login Faild !")
+            }
+            action.resetForm();
+        })
+    })
+
+
 
 
 
@@ -57,18 +74,36 @@ const Register = () => {
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Sign up to your account
                             </h1>
-                            <form className="space-y-4 md:space-y-6" onSubmit={SignUp} method='POST'>
+                            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} method='POST'>
                                 <div>
                                     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                                    <input type="text" onChange={handlername} name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required />
+                                    <input type="text"
+                                        name="name"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.name}
+                                        id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required />
+                                    {errors.name && touched.name ? <p className="text-danger">{errors.name}</p> : null}
                                 </div>
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" onChange={handleremail} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                    <input type="email"
+                                        name="email"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                                    {errors.email && touched.email ? <p className="text-danger">{errors.email}</p> : null}
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" onChange={handlerpassword} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <input type="password"
+                                        name="password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                        id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    {errors.password && touched.password ? <p className="text-danger">{errors.password}</p> : null}
                                 </div>
                                 <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign up</button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
